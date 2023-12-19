ae27a663928c9aad6d1d6a4785ba0f2e6266a1b7
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-12-19 09:33:30 -0800

    Add SignIn.js, update App+Header.js to accomodate routing.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ticketsImage from "./../img/tickets.jpg";
+import { Link } from "react-router-dom";
 
 function Header(){
   return(
     <React.Fragment>
-      <h1>Help Queue</h1>
+			<h1>Help Queue</h1>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/sign-in">Sign In</Link>
+				</li>
+				</ul>
       <img height="300px" src={ticketsImage} alt="A roll of tickets"/>
     </React.Fragment>
   );
