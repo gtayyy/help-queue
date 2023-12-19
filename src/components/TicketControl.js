bd77e3c246aa7d5ea4adbc11c4cf4193a67a859d
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-12-19 10:34:18 -0800

    Add authorization of sign in to restrict access

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -3,7 +3,7 @@ import NewTicketForm from "./NewTicketForm";
 import TicketList from "./TicketList";
 import EditTicketForm from "./EditTicketForm";
 import TicketDetail from "./TicketDetail";
-import { db } from './../firebase.js'; 					// add {} b/c it's now a named export (not default)
+import { db, auth } from './../firebase.js'; 					// add {} b/c both are now a named export (not default)
 import { collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc } from "firebase/firestore";
 
 
@@ -16,21 +16,21 @@ function TicketControl() {
 
 	const [error, setError] = useState(null);
 
-	useEffect(() => { 
-    const unSubscribe = onSnapshot(
-      collection(db, "tickets"), 
-      (collectionSnapshot) => {
-        const tickets = [];
-        collectionSnapshot.forEach((doc) => {
-            tickets.push({
-              names: doc.data().names, 
-              location: doc.data().location, 
-              issue: doc.data().issue, 
-              id: doc.id
-            });
-        });
-        setMainTicketList(tickets);
-      }, 
+	useEffect(() => {
+		const unSubscribe = onSnapshot(
+			collection(db, "tickets"),
+			(collectionSnapshot) => {
+				const tickets = [];
+				collectionSnapshot.forEach((doc) => {
+					tickets.push({
+						names: doc.data().names,
+						location: doc.data().location,
+						issue: doc.data().issue,
+						id: doc.id
+					});
+				});
+				setMainTicketList(tickets);
+			},
 			(error) => {
 				setError(error.message);
 			}
@@ -56,9 +56,9 @@ function TicketControl() {
 	// }
 	
 	const handleDeletingTicket = async (id) => {
-    await deleteDoc(doc(db, "tickets", id));
-    setSelectedTicket(null);
-	} 
+		await deleteDoc(doc(db, "tickets", id));
+		setSelectedTicket(null);
+	}
 	
 	const handleEditClick = () => {
 		setEditing(true);
@@ -74,10 +74,10 @@ function TicketControl() {
 	// }
 
 	const handleEditingTicketInList = async (ticketToEdit) => {
-    const ticketRef = doc(db, "tickets", ticketToEdit.id);
-    await updateDoc(ticketRef, ticketToEdit);
-    setEditing(false);
-    setSelectedTicket(null);
+		const ticketRef = doc(db, "tickets", ticketToEdit.id);
+		await updateDoc(ticketRef, ticketToEdit);
+		setEditing(false);
+		setSelectedTicket(null);
 	}
 	
 	// const handleAddingNewTicketToList = (newTicket) => {
@@ -85,54 +85,63 @@ function TicketControl() {
 	// 	setMainTicketList(newMainTicketList);
 	// 	setFormVisibleOnPage(false)
 	// }
-  const handleAddingNewTicketToList = async (newTicketData) => {
-    const collectionRef = collection(db, "tickets");
-    await addDoc(collectionRef, newTicketData);
-    setFormVisibleOnPage(false);
-  }
+	const handleAddingNewTicketToList = async (newTicketData) => {
+		const collectionRef = collection(db, "tickets");
+		await addDoc(collectionRef, newTicketData);
+		setFormVisibleOnPage(false);
+	}
 	
 	const handleChangingSelectedTicket = (id) => {
 		const selection = mainTicketList.filter(ticket => ticket.id === id)[0];
 		setSelectedTicket(selection);
 	}
 
-	let currentlyVisibleState = null;
-  let buttonText = null; 
+	if (auth.currentUser == null) {								// here ---> 
+		return (
+			<React.Fragment>
+				<h1>You must be signed in to access the queue.</h1>
+			</React.Fragment>
+		)
+	} else if (auth.currentUser != null) {			// to here added to handle authorization
+		
+		let currentlyVisibleState = null;
+		let buttonText = null;
 
-	if (error) {
-		currentlyVisibleState = <p>There was an error: {error}</p>
-	} else if (editing) {      
-    currentlyVisibleState = 
-      <EditTicketForm 
-        ticket = {selectedTicket} 
-        onEditTicket = {handleEditingTicketInList} />;
-    buttonText = "Return to Ticket List";
-  } else if (selectedTicket != null) {
-    currentlyVisibleState = 
-      <TicketDetail 
-        ticket={selectedTicket} 
-        onClickingDelete={handleDeletingTicket}
-        onClickingEdit = {handleEditClick} />;
-    buttonText = "Return to Ticket List";
-  } else if (formVisibleOnPage) {
-    currentlyVisibleState = 
-      <NewTicketForm 
-        onNewTicketCreation={handleAddingNewTicketToList}/>;
-    buttonText = "Return to Ticket List"; 
-  } else {
-    currentlyVisibleState = 
-      <TicketList 
-        onTicketSelection={handleChangingSelectedTicket} 
-        ticketList={mainTicketList} />;
-    buttonText = "Add Ticket"; 
-  }
+		if (error) {
+			currentlyVisibleState = <p>There was an error: {error}</p>
+		} else if (editing) {
+			currentlyVisibleState =
+				<EditTicketForm
+					ticket={selectedTicket}
+					onEditTicket={handleEditingTicketInList} />;
+			buttonText = "Return to Ticket List";
+		} else if (selectedTicket != null) {
+			currentlyVisibleState =
+				<TicketDetail
+					ticket={selectedTicket}
+					onClickingDelete={handleDeletingTicket}
+					onClickingEdit={handleEditClick} />;
+			buttonText = "Return to Ticket List";
+		} else if (formVisibleOnPage) {
+			currentlyVisibleState =
+				<NewTicketForm
+					onNewTicketCreation={handleAddingNewTicketToList} />;
+			buttonText = "Return to Ticket List";
+		} else {
+			currentlyVisibleState =
+				<TicketList
+					onTicketSelection={handleChangingSelectedTicket}
+					ticketList={mainTicketList} />;
+			buttonText = "Add Ticket";
+		}
 
-  return (
-    <React.Fragment>
-      {currentlyVisibleState}
-			{error ? null : <button onClick={handleClick}>{buttonText}</button>}
-    </React.Fragment>
-  );
+		return (
+			<React.Fragment>
+				{currentlyVisibleState}
+				{error ? null : <button onClick={handleClick}>{buttonText}</button>}
+			</React.Fragment>
+		);
+	}
 }
 
 export default TicketControl;
