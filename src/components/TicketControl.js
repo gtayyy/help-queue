596e6d2febfb5b75cc51342b142526d818cf550d
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-12-18 16:05:17 -0800

    Implement edit/delete functionality in firestore

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -4,7 +4,7 @@ import TicketList from "./TicketList";
 import EditTicketForm from "./EditTicketForm";
 import TicketDetail from "./TicketDetail";
 import db from './../firebase.js';
-import { collection, addDoc, onSnapshot } from "firebase/firestore";
+import { collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc } from "firebase/firestore";
 
 
 function TicketControl() {
@@ -49,23 +49,35 @@ function TicketControl() {
 		}
 	}
 	
-	const handleDeletingTicket = (id) => {
-		const newMainTicketList = mainTicketList.filter(ticket => ticket.id !== id);
-		setMainTicketList(newMainTicketList);
-		setSelectedTicket(null);
-	}
+	// const handleDeletingTicket = (id) => {
+	// 	const newMainTicketList = mainTicketList.filter(ticket => ticket.id !== id);
+	// 	setMainTicketList(newMainTicketList);
+	// 	setSelectedTicket(null);
+	// }
+	
+	const handleDeletingTicket = async (id) => {
+    await deleteDoc(doc(db, "tickets", id));
+    setSelectedTicket(null);
+	} 
 	
 	const handleEditClick = () => {
 		setEditing(true);
 	}
 	
-	const handleEditingTicketInList = (ticketToEdit) => {
-		const editedMainTicketList = mainTicketList
-			.filter(ticket => ticket.id !== selectedTicket.id)
-			.concat(ticketToEdit);
-		setMainTicketList(editedMainTicketList);
-		setEditing(false);
-		setSelectedTicket(null);
+	// const handleEditingTicketInList = (ticketToEdit) => {
+	// 	const editedMainTicketList = mainTicketList
+	// 		.filter(ticket => ticket.id !== selectedTicket.id)
+	// 		.concat(ticketToEdit);
+	// 	setMainTicketList(editedMainTicketList);
+	// 	setEditing(false);
+	// 	setSelectedTicket(null);
+	// }
+
+	const handleEditingTicketInList = async (ticketToEdit) => {
+    const ticketRef = doc(db, "tickets", ticketToEdit.id);
+    await updateDoc(ticketRef, ticketToEdit);
+    setEditing(false);
+    setSelectedTicket(null);
 	}
 	
 	// const handleAddingNewTicketToList = (newTicket) => {
